eba152603975c821cff32643f6e178728d64b7db
Jose Alvarez <jealv@icloud.com>
2024-10-02 13:54:01 -0400

    Move mods

--- keyboards/boardsource/unicorne/keymaps/jose-elias-alvarez/keys.h
+++ keyboards/boardsource/unicorne/keymaps/jose-elias-alvarez/keys.h
@@ -7,12 +7,12 @@
 
 
 // home row mods
-#define MOD_A MT(MOD_LSFT, KC_A)
+#define MOD_A MT(MOD_LALT, KC_A)
 #define MOD_S MT(MOD_LCTL, KC_S)
-#define MOD_D MT(MOD_LALT, KC_D)
+#define MOD_D MT(MOD_LSFT, KC_D)
 #define MOD_F MT(MOD_LGUI, KC_F)
 
 #define MOD_J MT(MOD_RGUI, KC_J)
-#define MOD_K MT(MOD_RALT, KC_K)
+#define MOD_K MT(MOD_RSFT, KC_K)
 #define MOD_L MT(MOD_RCTL, KC_L)
-#define MOD_SC MT(MOD_RSFT, KC_SCLN)
+#define MOD_SC MT(MOD_RALT, KC_SCLN)
