cc2f4b6bfad6df32e8420dcc8c338979e926ffc6
Jose Alvarez <jealv@icloud.com>
2024-10-09 14:39:26 -0400

    Add combo to save USB-C plug

--- keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
 
+const uint16_t PROGMEM bootloader_combo[] = {KC_BTN4, KC_BTN2, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(bootloader_combo, QK_BOOT),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clockwise starting from upper left
     [0] = LAYOUT(KC_BTN4, KC_BTN5, DRAG_SCROLL, KC_BTN2, KC_BTN1, KC_BTN3)};
