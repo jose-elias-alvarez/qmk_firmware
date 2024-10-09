3cb7271653a2b9a1cd8025adbb8e316fad40827b
Jose Alvarez <jealv@icloud.com>
2024-10-09 14:35:19 -0400

    Disable drag scroll on any click

--- keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
@@ -3,3 +3,17 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clockwise starting from upper left
     [0] = LAYOUT(KC_BTN4, KC_BTN5, DRAG_SCROLL, KC_BTN2, KC_BTN1, KC_BTN3)};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // keep default handling
+        case DRAG_SCROLL:
+            return true;
+        default:
+            if (is_drag_scroll) {
+                is_drag_scroll = false;
+                return false;
+            }
+    }
+    return true;
+}
--- keyboards/ploopyco/ploopyco.h
+++ keyboards/ploopyco/ploopyco.h
@@ -37,3 +37,5 @@ enum ploopy_keycodes {
 
 bool encoder_update_user(uint8_t index, bool clockwise);
 bool encoder_update_kb(uint8_t index, bool clockwise);
+
+bool is_drag_scroll;
