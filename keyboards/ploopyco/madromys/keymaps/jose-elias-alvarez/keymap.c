eac093e30f10be03170e35b520e573b6c89a7066
Jose Alvarez <jealv@icloud.com>
2024-10-10 14:22:18 -0400

    Try momentary drag scroll

--- keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/jose-elias-alvarez/keymap.c
@@ -10,17 +10,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clockwise starting from upper left
     [0] = LAYOUT(KC_BTN4, KC_BTN5, KC_BTN3, DRAG_SCROLL, KC_BTN1, KC_BTN2),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        // keep default handling
-        case DRAG_SCROLL:
-            return true;
-        default:
-            if (is_drag_scroll) {
-                is_drag_scroll = false;
-                return false;
-            }
-    }
-    return true;
-}
