6965bb129de26bb13de3d3aff3923b7076f354f4
Jose Alvarez <jose@alv.email>
2025-11-22 12:55:02 -0500

    Add delay to Globe + inline

--- keyboards/keychron/q1v2/ansi_encoder/keymaps/jose_elias_alvarez/keymap.c
+++ keyboards/keychron/q1v2/ansi_encoder/keymaps/jose_elias_alvarez/keymap.c
@@ -4,6 +4,12 @@
 
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 
+enum custom_keycodes {
+    GLB = SAFE_RANGE,
+};
+static uint16_t delay_globe_timer = 0;
+static bool     delay_globe_armed = false;
+
 enum layers {
     _BASE,
     _GAME,
@@ -52,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         CTL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         OS_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              CAPS,     KC_UP,
-        KC_GLB,   KC_LALT,  KC_LGUI,                                NAV,                                    KC_RGUI,  FN,         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        GLB,      KC_LALT,  KC_LGUI,                                NAV,                                    KC_RGUI,  FN,         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_GAME] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_MUTE,
@@ -94,6 +100,28 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return true;
         default:
             return false;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case GLB:
+            if (record->event.pressed) {
+                delay_globe_timer = timer_read();
+                delay_globe_armed = true;
+            } else {
+                delay_globe_armed = false;
+            }
+            return false;
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (delay_globe_armed && timer_elapsed(delay_globe_timer) > 100) {
+        host_consumer_send(AC_NEXT_KEYBOARD_LAYOUT_SELECT);
+        host_consumer_send(0);
+        delay_globe_armed = false;
     }
 }
 
