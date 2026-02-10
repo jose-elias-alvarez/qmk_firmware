7aca05ba0000fc7bd15f232e5a8c3a193c97ffc3
Jose Alvarez <jose@alv.email>
2026-02-10 12:24:58 -0500

    Change modifier, add modeline

--- keyboards/keychron/q1v2/ansi_encoder/keymaps/jose_elias_alvarez/keymap.c
+++ keyboards/keychron/q1v2/ansi_encoder/keymaps/jose_elias_alvarez/keymap.c
@@ -1,3 +1,4 @@
+// vim: set nowrap:
 #include "process_combo.h"
 #include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         CTL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         OS_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              CAPS,     KC_UP,
-        GLB,      KC_LALT,  KC_LGUI,                                NAV,                                    KC_RGUI,  FN,         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        GLB,      KC_LALT,  KC_LGUI,                                NAV,                                    KC_RALT,  FN,         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_GAME] = LAYOUT_ansi_82(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_MUTE,
@@ -66,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_END,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  FN,         KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RALT,  FN,         KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_NAV] = LAYOUT_ansi_82(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
@@ -74,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         KC_LCTL,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,              _______,            _______,
         KC_LSFT,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              KC_RSFT,  _______,
-        KC_LCTL,  KC_LALT,  KC_LGUI,                                _______,                                KC_RGUI,  _______,    KC_RCTL,  _______,  _______,  _______),
+        KC_LCTL,  KC_LALT,  KC_LGUI,                                _______,                                KC_RALT,  _______,    KC_RCTL,  _______,  _______,  _______),
 
     [_FN] = LAYOUT_ansi_82(
         _______,  KC_BRIU,  KC_BRID,  KC_MCTL,  KC_LPAD,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  QK_BOOT,            _______,
